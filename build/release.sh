9954aeb17a588837e39b9ffc9e21b2d4d4479e61
heora <yangzhiqiang@winwin-inc.com>
2022-05-19 16:45:59 +0800

    perf: 增加构建脚本

---
+++ build/release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  if [[ `git status --porcelain` ]];
+  then
+    git add -A
+    git commit -am "build: compile $VERSION"
+  fi
+
+  # commit
+  npm version $VERSION --message "release: $VERSION"
+
+  # publish
+  git push origin main
+  git push origin refs/tags/v$VERSION
+fi
