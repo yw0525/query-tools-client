f628610adb295bad6633c17f2f80ca75c7e92f4d
heora <yangzhiqiang@winwin-inc.com>
2022-05-19 17:16:04 +0800

    build: compile 1.0.0

--- build/release.sh
+++ build/release.sh
@@ -7,11 +7,10 @@ read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  # commit
   cd release/app
   npm version $VERSION --message "release: $VERSION"
 
-  # after commit
+  # commit
   if [[ `git status --porcelain` ]];
   then
     git add -A
@@ -20,5 +19,6 @@ then
 
   # publish
   git push origin main
+  git tag v$VERSION
   git push origin refs/tags/v$VERSION
 fi
